f05431b2068362b97ffc88d85068a774fbc60cb0
Duncan Brewster <duncanwbrewster@gmail.com>
2022-04-21 10:31:19 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,16 @@ import { Button } from "./components/button";
 import { Body, Container } from "./components/container";
 import { Title, AppSearchForm, Input } from "./components/form";
 
-
-
-
 function App() {
   const [query, setQuery] = useState("");
   const [recipes, setRecipes] = useState([]);
 
   const ID = /* process.env.REACT_APP_ID */ "b512b6e5";
-  const Key = /* process.env.REACT_APP_KEY */ "47a00943b5464e3281ba5a9d0575fd2d";
+  const Key =
+    /* process.env.REACT_APP_KEY */ "47a00943b5464e3281ba5a9d0575fd2d";
 
   const url = `https://api.edamam.com/search?q=${query}&app_id=${ID}&app_key=${Key}&from=0&to=20`;
-  
+
   async function getRecipes() {
     const result = await Axios.get(url);
     setRecipes(result.data.hits);
@@ -27,34 +25,30 @@ function App() {
   const submit = (e) => {
     e.preventDefault();
     getRecipes();
-  }
-  
+  };
 
   return (
-    
-      <Body>
-        <Background />
-        <Container>
-          <Title>Recipe Finder</Title>
-          <AppSearchForm onSubmit={submit}>
-            <Input 
-              type="text" 
-              placeholder="enter ingredient" 
-              value={query} 
-              onChange={(e) => setQuery(e.target.value)} 
-            />
-            <Button type="submit">Search</Button>
-          </AppSearchForm>
+    <Body>
+      <Background />
+      <Container>
+        <Title>Recipe Finder</Title>
+        <AppSearchForm onSubmit={submit}>
+          <Input
+            type="text"
+            placeholder="enter ingredient"
+            value={query}
+            onChange={(e) => setQuery(e.target.value)}
+          />
+          <Button type="submit">Search</Button>
+        </AppSearchForm>
 
-          <RecipeContainer>
-            {recipes.map(recipe => {
-              return <RecipeTile recipe={recipe}/>;
-            })}
-          </RecipeContainer>
-        </Container>
-        
-      </Body>
-    
+        <RecipeContainer>
+          {recipes.map((recipe) => {
+            return <RecipeTile recipe={recipe} />;
+          })}
+        </RecipeContainer>
+      </Container>
+    </Body>
   );
 }
 
